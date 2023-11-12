9755fe8930e013a5dca45c49cc9fea0b4f239bda
AnimationT <86012532+AnimationT@users.noreply.github.com>
2023-11-12 19:11:46 +0000

    Home page and operation memego finished

---
+++ script.js
@@ -0,0 +1,11 @@
+function ytClick() {
+    window.location="https://www.youtube.com/channel/UC6xBKjSPeUSJ-iNiBuojg1g";
+}
+
+function ttClick() {
+    window.location="https://www.tiktok.com/@meme.co9569";
+}
+
+function opMem() {
+    window.location="/memego";
+}
